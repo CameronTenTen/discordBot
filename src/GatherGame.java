b405ff64d8e74455a8ff96dec1326c9819c33cca
cameron <cameronh1010@gmail.com>
2017-09-06 20:18:37 +0930

    start of a gather game object for when game is more managed

---
+++ src/GatherGame.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class GatherGame
+{
+	ArrayList<PlayerObject> bluePlayerList;
+	ArrayList<PlayerObject> redPlayerList;
+	
+	public boolean shuffleTeams()
+	{
+		return false;
+	}
+	
+	
+}
