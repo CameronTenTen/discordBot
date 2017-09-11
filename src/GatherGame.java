f6892b2b213f9a3d62ec3c088f1e45dcd3ba5373
cameron <cameronh1010@gmail.com>
2017-09-11 17:44:13 +0930

    Expanded GatherGame object implementation

--- src/GatherGame.java
+++ src/GatherGame.java
@@ -1,14 +1,121 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import sx.blah.discord.handle.obj.IUser;
 
 public class GatherGame
 {
-	ArrayList<PlayerObject> bluePlayerList;
-	ArrayList<PlayerObject> redPlayerList;
+	//currently unused
+	private int gameID;
+	private int currentRound;
+	private gameState currState;
 	
-	public boolean shuffleTeams()
+	private List<PlayerObject> players;
+	private List<PlayerObject> bluePlayerList;
+	private List<PlayerObject> redPlayerList;
+	private GatherServer server;
+	enum gameState
 	{
-		return false;
+		pregame,
+		roundInProgress,
+		ended
 	}
 	
+	GatherGame(int matchId, List<PlayerObject> players, List<PlayerObject> blueTeam, List<PlayerObject> redTeam, GatherServer server)
+	{
+		this.gameID = matchId;
+		this.players = players;
+		this.bluePlayerList = blueTeam;
+		this.redPlayerList = redTeam;
+		this.server = server;
+	}
 	
+	public void shuffleTeams()
+	{
+		Collections.shuffle(players);
+		bluePlayerList = players.subList(0, players.size()/2);
+		redPlayerList = players.subList(players.size()/2, players.size());
+	}
+	
+	public List<String> blueMentionList()
+	{
+		ArrayList<String> list = new ArrayList<String>();
+		for(PlayerObject player : bluePlayerList)
+		{
+			list.add(player.getMentionString());
+		}
+		return list;
+	}
+	
+	public List<String> redMentionList()
+	{
+		ArrayList<String> list = new ArrayList<String>();
+		for(PlayerObject player : redPlayerList)
+		{
+			list.add(player.getMentionString());
+		}
+		return list;
+	}
+
+	public int getGameID() {
+		return gameID;
+	}
+
+	public void setGameId(int gameID) {
+		this.gameID = gameID;
+	}
+	
+	public int getPlayerTeam(IUser user)
+	{
+		System.out.print(redPlayerList.size());
+		for(PlayerObject p : bluePlayerList)
+		{
+			if(p.getDiscordUserInfo().equals(user))return 0;
+		}
+		for(PlayerObject p : redPlayerList)
+		{
+			if(p.getDiscordUserInfo().equals(user))return 1;
+		}
+		return -1;
+	}
+	
+	public int getPlayerTeam(PlayerObject player)
+	{
+		return getPlayerTeam(player.getDiscordUserInfo());
+	}
+	
+	public boolean isPlayerPlaying(IUser user)
+	{
+		return getPlayerTeam(user)!=-1;
+	}
+	
+	public boolean isPlayerPlaying(PlayerObject player)
+	{
+		return getPlayerTeam(player)!=-1;
+	}
+
+	public String getServerIp()
+	{
+		return server.getIp();
+	}
+	
+	public int getServerPort()
+	{
+		return server.getPort();
+	}
+
+	public GatherServer getServer() {
+		return server;
+	}
+
+	public void setServer(GatherServer server) {
+		this.server = server;
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		return this.server.equals(((GatherGame)obj).server);
+	}
 }
