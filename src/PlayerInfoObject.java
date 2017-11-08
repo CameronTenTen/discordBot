e7f3170df7f3eb3839bdde837bfdb921af97a8e8
cameron <cameronh1010@gmail.com>
2017-11-08 19:16:46 +1030

    Added Javadoc for PlayerInfoObject

--- src/PlayerInfoObject.java
+++ src/PlayerInfoObject.java
@@ -1,4 +1,8 @@
 
+/**Object for holding player info, used to check if a username someone is trying to link with is valid, and to check the correct case of that username. 
+ * @author cameron
+ *
+ */
 public class PlayerInfoObject {
 	boolean gold;
 	String username;
