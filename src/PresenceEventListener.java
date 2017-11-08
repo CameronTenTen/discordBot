8de1ca0c1f09f248df81328882e7cb79a661c734
cameron <cameronh1010@gmail.com>
2017-11-08 19:15:39 +1030

    Added Javadoc for PresenceEventListener

--- src/PresenceEventListener.java
+++ src/PresenceEventListener.java
@@ -3,9 +3,17 @@ import sx.blah.discord.handle.impl.events.user.PresenceUpdateEvent;
 import sx.blah.discord.handle.obj.IPresence;
 import sx.blah.discord.handle.obj.StatusType;
 
+/**Listener for when a user changes presence state (offline/away/online). 
+ * @author cameron
+ * @see PresenceEventListener#handle(PresenceUpdateEvent)
+ */
 public class PresenceEventListener implements IListener<PresenceUpdateEvent>
 {
 
+	/**The function that is called when the listener is triggered
+	 * @param event the event object, contains both new and old presence states
+	 * @see sx.blah.discord.api.events.IListener#handle(sx.blah.discord.api.events.Event)
+	 */
 	@Override
 	public void handle(PresenceUpdateEvent event)
 	{
