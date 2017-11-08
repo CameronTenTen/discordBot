68a6749b62bcd8f422b1a0ce6385fd7d997d7c3e
cameron <cameronh1010@gmail.com>
2017-11-08 18:45:41 +1030

    Added Javadoc for CommandReconnect

--- src/CommandReconnect.java
+++ src/CommandReconnect.java
@@ -2,8 +2,19 @@ import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import sx.blah.discord.handle.obj.IMessage;
 
+/**Rough admin command for forcing the discord bot to disconnect and reconnect to all KAG servers it is supposed to be connected to. Returns no feedback to discord. Must be used in command channel. 
+ * @author cameron
+ * @see GatherObject#disconnectKAGServers()
+ * @see GatherObject#connectKAGServers()
+ */
 public class CommandReconnect implements CommandExecutor
 {
+	/**The function that is called when the command is used
+	 * @param message
+	 * @param args
+	 * @see https://github.com/BtoBastian/sdcf4j
+	 * @see #CommandReconnect
+	 */
 	@Command(aliases = {"!reconnect"}, description = "Admin only - reconnect to the kag servers")
 	public void onCommand(IMessage message, String[] args)
 	{
