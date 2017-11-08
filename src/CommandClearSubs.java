e2dad7ace24e662aec237e599d5abffc0f9c58e7
cameron <cameronh1010@gmail.com>
2017-11-08 18:40:04 +1030

    Added Javadoc for CommandClearSubs

--- src/CommandClearSubs.java
+++ src/CommandClearSubs.java
@@ -2,8 +2,19 @@ import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import sx.blah.discord.handle.obj.IMessage;
 
+/**
+ * Admin only command for clearing all current sub requests. Must be used in command channel. 
+ * Calls GatherObject.SubstitutionObject.ClearSubs()
+ * @author cameron
+ * @see SubstitutionObject#ClearSubs()
+ */
 public class CommandClearSubs implements CommandExecutor
 {
+	/**The function that is called when the command is used
+	 * @param message
+	 * @see https://github.com/BtoBastian/sdcf4j
+	 * @see #CommandClearSubs
+	 */
 	@Command(aliases = {"!clearsubs"}, description = "Admin only - clear all current sub requests")
 	public void onCommand(IMessage message)
 	{
