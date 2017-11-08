3a29f6a337d0f5c515233169192f10d895ae7bec
cameron <cameronh1010@gmail.com>
2017-11-08 18:51:27 +1030

    Added Javadoc for CommandStatus

--- src/CommandStatus.java
+++ src/CommandStatus.java
@@ -2,8 +2,17 @@ import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import sx.blah.discord.handle.obj.IMessage;
 
+/**Command for checking the status of any currently running games. Must be used in command channel. 
+ * @author cameron
+ * @see GatherObject#statusString()
+ */
 public class CommandStatus implements CommandExecutor
 {
+	/**The function that is called when the command is used
+	 * @param message
+	 * @see https://github.com/BtoBastian/sdcf4j
+	 * @see #CommandStatus
+	 */
 	@Command(aliases = {"!status"}, description = "Check status of current games")
 	public void onCommand(IMessage message)
 	{
