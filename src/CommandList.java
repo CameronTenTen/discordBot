c75189f96c3b5e5c35ba5c84d4c92ab656c569ba
cameron <cameronh1010@gmail.com>
2017-11-08 18:43:16 +1030

    Added Javadoc for CommandList

--- src/CommandList.java
+++ src/CommandList.java
@@ -2,8 +2,18 @@ import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import sx.blah.discord.handle.obj.IMessage;
 
+/**Command for saying the current player list and queue size for the gather object associated with this channel.  Must be used in command channel. 
+ * @author cameron
+ * @see #GatherQueueObject
+ * @see GatherObject#queueString()
+ */
 public class CommandList implements CommandExecutor
 {
+	/**The function that is called when the command is used
+	 * @param message
+	 * @see https://github.com/BtoBastian/sdcf4j
+	 * @see #CommandList
+	 */
 	@Command(aliases = {"!list"}, description = "Check the current player list")
 	public void onCommand(IMessage message)
 	{
