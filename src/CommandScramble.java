bbba444a699485fa74e1cfa16ce5c7bcb4f242be
cameron <cameronh1010@gmail.com>
2017-11-08 18:47:01 +1030

    Added Javadoc for CommandScramble

--- src/CommandScramble.java
+++ src/CommandScramble.java
@@ -2,9 +2,17 @@ import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import sx.blah.discord.handle.obj.IMessage;
 
+/**Command for voting to scramble the teams of a game. Must be used in command channel. 
+ * @author cameron
+ * @see GatherObject#addScrambleVote(PlayerObject)
+ */
 public class CommandScramble implements CommandExecutor
 {
-	
+	/**The function that is called when the command is used
+	 * @param message
+	 * @see https://github.com/BtoBastian/sdcf4j
+	 * @see #CommandScramble
+	 */
 	@Command(aliases = {"!scramble"}, description = "Scramble the teams")
 	public void onCommand(IMessage message)
 	{
