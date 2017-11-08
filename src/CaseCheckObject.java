7691f3d2dab7adf7ef9bf53f0d2e25645dc4055b
cameron <cameronh1010@gmail.com>
2017-11-08 18:39:07 +1030

    Added Javadoc for CaseCheckObject

--- src/CaseCheckObject.java
+++ src/CaseCheckObject.java
@@ -1,4 +1,9 @@
 
+/**
+ * Holds a playerInfo object for use when checking if a player exists on the kag api and checking the correct case for their name
+ * @author cameron
+ *
+ */
 public class CaseCheckObject {
 	PlayerInfoObject playerInfo;
 }
