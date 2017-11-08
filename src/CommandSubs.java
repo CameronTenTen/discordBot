1592681d16263b5ece123cdf89a4f17d27d91a5b
cameron <cameronh1010@gmail.com>
2017-11-08 18:54:58 +1030

    Added Javadoc for CommandSubs

--- src/CommandSubs.java
+++ src/CommandSubs.java
@@ -2,8 +2,17 @@ import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import sx.blah.discord.handle.obj.IMessage;
 
+/**Command for checking the current sub requests. Must be used in command channel. 
+ * @author cameron
+ * @see SubManager#toString()
+ */
 public class CommandSubs implements CommandExecutor
 {
+	/**The function that is called when the command is used
+	 * @param message
+	 * @see https://github.com/BtoBastian/sdcf4j
+	 * @see #CommandSubs
+	 */
 	@Command(aliases = {"!subs"}, description = "Check current sub requests")
 	public void onCommand(IMessage message)
 	{
