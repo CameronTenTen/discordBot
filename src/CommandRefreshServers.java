5a90efc8b79f6bc59dccaa77039cd9849e4b7a46
cameron <cameronh1010@gmail.com>
2017-11-08 18:45:56 +1030

    Added Javadoc for CommandRefreshServers

--- src/CommandRefreshServers.java
+++ src/CommandRefreshServers.java
@@ -4,8 +4,19 @@ import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import sx.blah.discord.handle.obj.IMessage;
 
+/**Command for sending the current player lists to the appropriate servers. Must be used in command channel. 
+ * <p>
+ * Useful for when a server was not connected when a game started or when a sub was made. 
+ * @author cameron
+ * @see GatherGame#updateTeamsOnServer()
+ */
 public class CommandRefreshServers implements CommandExecutor
 {
+	/**The function that is called when the command is used
+	 * @param message
+	 * @see https://github.com/BtoBastian/sdcf4j
+	 * @see #CommandRefreshServers
+	 */
 	@Command(aliases = {"!refreshservers"}, description = "Refresh the player list in currently running games, useful in case of a server disconnect")
 	public void onCommand(IMessage message)
 	{
