b8462f8bd76be80f30364fe5dc1c55b07cb41ecd
cameron <cameronh1010@gmail.com>
2017-11-08 18:50:42 +1030

    Added Javadoc for CommandStart

--- src/CommandStart.java
+++ src/CommandStart.java
@@ -2,8 +2,17 @@ import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import sx.blah.discord.handle.obj.IMessage;
 
+/**Admin only command for sending all players in the gather general voice channel into their team rooms. Must be used in command channel. 
+ * @author cameron
+ *@see GatherObject#movePlayersIntoTeamRooms()
+ */
 public class CommandStart implements CommandExecutor
 {
+	/**The function that is called when the command is used
+	 * @param message
+	 * @see https://github.com/BtoBastian/sdcf4j
+	 * @see #CommandStart
+	 */
 	@Command(aliases = {"!start"}, description = "Admin only - move users from general chat to team rooms")
 	public void onCommand(IMessage message)
 	{
