618dc39b10bdc5e892f31dc12d789e1fd466e7c8
cameron <cameronh1010@gmail.com>
2017-11-08 19:16:21 +1030

    Added Javadoc for TokenCheckObject

--- src/TokenCheckObject.java
+++ src/TokenCheckObject.java
@@ -1,4 +1,10 @@
 
+/**Object used for holding token information when it is retreived from the kag2d api. 
+ * @author cameron
+ * @see #CommandLink
+ * @see https://developers.thd.vg/api/players.html
+ * @see https://api.kag2d.com/v1/player/username/token/new
+ */
 public class TokenCheckObject {
 	
 	TokenCheckObject()
