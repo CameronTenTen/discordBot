b42fd6025d66473fde1be5cf5f81ae98baed1255
cameron <cameronh1010@gmail.com>
2017-11-08 18:56:53 +1030

    Added Javadoc for DiscordBot object

--- src/DiscordBot.java
+++ src/DiscordBot.java
@@ -27,21 +27,44 @@ import sx.blah.discord.util.MissingPermissionsException;
 import sx.blah.discord.util.RequestBuffer;
 import sx.blah.discord.util.RequestBuilder;
 
+/**The main bot class. Contains most of the interaction with the Discord bot client. Contains various helper functions and objects.
+ * @author cameron
+ *
+ */
 public class DiscordBot {
 
-	public static IDiscordClient client; // The instance of the discord client.
+	/**
+	 * The instance of the Discord4J client
+	 */
+	public static IDiscordClient client;
+	/**
+	 * This builder object is used for making rate limit safe discord requests
+	 */
 	public RequestBuilder builder;
 	
 	public static DiscordBot bot;
 	
+	/**The database manipulation object
+	 * @see GatherDB
+	 */
 	public static GatherDB database;
+	/**The player object tracker
+	 * @see PlayerObjectManager
+	 */
 	public static PlayerObjectManager players;
 
-	//one gather object per guild
+	/**Set of gather objects. Each gather object represents one gather queue, and one command channel. 
+	 * @see GatherObject
+	 */
 	public static Set<GatherObject> gatherObjects;
 
 	public boolean doKagServerConnections = true;
 
+	/**Helper function for getting the correct gather object, helpful for when a player uses a command. 
+	 * @param channel - the channel being used
+	 * @return The GatherObject associated with the channel, or null if there is no object for this channel
+	 * @see GatherObject
+	 */
 	public static GatherObject getGatherObjectForChannel(IChannel channel) {
 		if(channel==null) return null;
 		for (GatherObject object : gatherObjects) {
@@ -51,6 +74,12 @@ public class DiscordBot {
 		return null;
 	}
 	
+	/**Helper function for getting the correct gather object when data or commands are sent from a connected KAG server. 
+	 * @param ip - the ip address of the server
+	 * @param port - the port of the server
+	 * @return The GatherObject this server is attached to, or null if the server couldnt be found
+	 * @see GatherObject
+	 */
 	public static GatherObject getGatherObjectForServer(String ip, int port)
 	{
 		for(GatherObject obj : gatherObjects)
@@ -64,11 +93,22 @@ public class DiscordBot {
 		return null;
 	}
 	
+	/**Wrapper function for {@link #getGatherObjectForServer(String, int)}
+	 * @param game - the GatherGame object
+	 * @return the GatherObject that contains this game
+	 * @see GatherGame
+	 * @see GatherObject
+	 */
 	public static GatherObject getGatherObjectForGame(GatherGame game)
 	{
 		return DiscordBot.getGatherObjectForServer(game.getServerIp(), game.getServerPort());
 	}
 
+	/**Initial bot setup goes here. Some things that require the bot to be setup fully to work are done in a ReadyEventListener. 
+	 * <p>
+	 * Some things that are done here include instantiation of the client and request builder, client login, registering event listeners, registering command listeners. 
+	 * @param token - the bot login token to be used for authentication
+	 */
 	public void startBot(String token) {
 		ClientBuilder builder = new ClientBuilder(); // Creates a new client builder instance
 
@@ -125,6 +165,10 @@ public class DiscordBot {
 		cmdHandler.registerCommand(new CommandStatus());
 	}
 
+	/**Main, instantiates some things, loads the database properties, sets up the database and player object managers
+	 * @param args
+	 * @throws IOException
+	 */
 	public static void main(String[] args) throws IOException {
 		bot = new DiscordBot();
 		gatherObjects = new HashSet<GatherObject>();
@@ -151,11 +195,28 @@ public class DiscordBot {
 	}
 
 	//wrappers for doing things in order to avoid rate limit exceptions
+
+	/**Wrapper function for sending messages without getting rate limit exceptions and with tts defaulted to false.
+	 * @param channel - the channel to put the message in
+	 * @param msg - the message to send
+	 * @return the message object after the message has been created, usually only want this if we are going to edit the message. 
+	 * @see #sendMessage(IChannel, String, boolean)
+	 * @see https://discord4j.readthedocs.io/en/latest/RateLimitExceptions-and-you/
+	 */
 	public static IMessage sendMessage(IChannel channel, String msg)
 	{
 		return sendMessage(channel, msg, false);
 	}
 	
+	/**Wrapper function for sending messages without getting rate limit exceptions. 
+	 * @param channel - the channel to put the message in
+	 * @param msg - the message to send
+	 * @param tts - should use text to speech
+	 * @return the message object after the message has been created, usually only want this if we are going to edit the message. 
+	 * @see Discord4J: {@link IChannel#sendMessage(String, boolean)}
+	 * @see https://discord4j.readthedocs.io/en/latest/RateLimitExceptions-and-you/
+	 * @see Discord4J: {@link RequestBuffer#request(sx.blah.discord.util.RequestBuffer.IRequest)}
+	 */
 	public static IMessage sendMessage(IChannel channel, String msg, boolean tts)
 	{
 		return RequestBuffer.request(() -> {
@@ -165,12 +226,20 @@ public class DiscordBot {
 			}
 			catch(NullPointerException e)
 			{
+				//this happened once???
 				Discord4J.LOGGER.warn("Null pointer exception caught from Discord4J code: " + e.getMessage());
 			}
 			return null;
 		}).get();
 	}
 	
+	/**Wrapper for editing messages without getting rate limit exceptions. 
+	 * @param msg - the message to be edited
+	 * @param newString - the new version of the message
+	 * @see Discord4J: {@link IMessage#edit(String)}
+	 * @see https://discord4j.readthedocs.io/en/latest/RateLimitExceptions-and-you/
+	 * @see Discord4J: {@link RequestBuffer#request(sx.blah.discord.util.RequestBuffer.IRequest)}
+	 */
 	public static void editMessage(IMessage msg, String newString)
 	{
 		RequestBuffer.request(() -> {
@@ -178,6 +247,13 @@ public class DiscordBot {
 		});
 	}
 	
+	/**Wrapper for replying to messages without getting rate limit exceptions. 
+	 * @param msg - the message to reply to
+	 * @param reply - the reply message
+	 * @see Discord4J: {@link IMessage#reply(String)}
+	 * @see https://discord4j.readthedocs.io/en/latest/RateLimitExceptions-and-you/
+	 * @see Discord4J: {@link RequestBuffer#request(sx.blah.discord.util.RequestBuffer.IRequest)}
+	 */
 	public static void reply(IMessage msg, String reply)
 	{
 		RequestBuffer.request(() -> {
@@ -185,6 +261,12 @@ public class DiscordBot {
 		});
 	}
 	
+	/**Wrapper for deleting messages without getting rate limit exceptions.
+	 * @param msg - the message to delete
+	 * @see Discord4J: {@link IMessage#delete()}
+	 * @see https://discord4j.readthedocs.io/en/latest/RateLimitExceptions-and-you/
+	 * @see Discord4J: {@link RequestBuffer#request(sx.blah.discord.util.RequestBuffer.IRequest)}
+	 */
 	public static void delete(IMessage msg)
 	{
 		RequestBuffer.request(() -> {
@@ -192,6 +274,13 @@ public class DiscordBot {
 		});
 	}
 	
+	/**Wrapper for adding roles to a user without getting rate limit exceptions. 
+	 * @param user - the user to be changed
+	 * @param role - the role to give them
+	 * @see Discord4J: {@link IUser#addRole(IRole)}
+	 * @see https://discord4j.readthedocs.io/en/latest/RateLimitExceptions-and-you/
+	 * @see Discord4J: {@link RequestBuffer#request(sx.blah.discord.util.RequestBuffer.IRequest)}
+	 */
 	public static void addRole(IUser user, IRole role)
 	{
 		if(user == null || role == null) return;
@@ -200,6 +289,13 @@ public class DiscordBot {
 		});
 	}
 	
+	/**Wrapper for removing roles from a user without getting rate limit exceptions. 
+	 * @param user - the user to be changed
+	 * @param role - the role to take away
+	 * @see Discord4J: {@link IUser#removeRole(IRole)}
+	 * @see https://discord4j.readthedocs.io/en/latest/RateLimitExceptions-and-you/
+	 * @see Discord4J: {@link RequestBuffer#request(sx.blah.discord.util.RequestBuffer.IRequest)}
+	 */
 	public static void removeRole(IUser user, IRole role)
 	{
 		if(user == null || role == null) return;
@@ -208,6 +304,13 @@ public class DiscordBot {
 		});
 	}
 	
+	/**Wrapper for moving a user to a voice channel without getting rate limit exceptions. User must already be in a voice channel to allow moving them. 
+	 * @param user - the user to be moved
+	 * @param channel - the channel to move them to
+	 * @see Discord4J: {@link IUser#moveToVoiceChannel(IVoiceChannel)}
+	 * @see https://discord4j.readthedocs.io/en/latest/RateLimitExceptions-and-you/
+	 * @see Discord4J: {@link RequestBuffer#request(sx.blah.discord.util.RequestBuffer.IRequest)}
+	 */
 	public static void moveToVoiceChannel(IUser user, IVoiceChannel channel)
 	{
 		RequestBuffer.request(() -> {
@@ -217,17 +320,32 @@ public class DiscordBot {
 			}
 			catch (DiscordException e)
 			{
+				//this can happen if the room doesnt exist or the user isnt already in a channel?
+				//or does it just happen if the bot doesnt have the permissions? I dont remember
 				Discord4J.LOGGER.warn(e.getMessage());
 			}
 		});
 	}
 
+	/**Wrapper for setting the bot "playing" text without getting rate limit exceptions. Playing text is global for all guilds the bot is in. 
+	 * @param newText - the new playing text to use
+	 * @see Discord4J: {@link IDiscordClient#changePlayingText(String)}
+	 * @see https://discord4j.readthedocs.io/en/latest/RateLimitExceptions-and-you/
+	 * @see Discord4J: {@link RequestBuffer#request(sx.blah.discord.util.RequestBuffer.IRequest)}
+	 */
 	public static void setPlayingText(String newText) {
 		RequestBuffer.request(() -> {
 			client.changePlayingText(newText);
 		});
 	}
 
+	/**Wrapper for setting the title of a text channel without getting rate limit exceptions. Does some basic parsing of the input text to remove spaces, but prints an error for other invalid characters. 
+	 * <p>
+	 * Discord only allows alphanumeric characters, dashes, and underscores in text channel names. 
+	 * @param guild the guild containing the channel
+	 * @param channel the channel to be changed
+	 * @param newText the new channel name
+	 */
 	public static void setChannelCaption(IGuild guild, IChannel channel, String newText) {
 		// remove spaces from the string (spaces not allowed)
 		// TODO: remove other illegal characters instead of just catching the exception
@@ -243,6 +361,10 @@ public class DiscordBot {
 		}
 	}
 
+	/**Does the things needed when a player disconnects. As of writing this it only removes them from any queue they might be in. Called by the PresenceEventListener when a user changes presence state. 
+	 * @param user the user that disconnected
+	 * @see #PresenceEventListener
+	 */
 	public static void userWentOffline(IUser user) {
 		for (GatherObject object : gatherObjects) {
 			if (object.remFromQueue(user) == 1) {
@@ -254,20 +376,12 @@ public class DiscordBot {
 		}
 	}
 	
-	public static List<PlayerObject> linkRequests;
-	
-	public static PlayerObject getDiscordLinkRequest(IUser user)
-	{
-		for(PlayerObject p : linkRequests)
-		{
-			if(p.getDiscordUserInfo().equals(user))
-			{
-				return p;
-			}
-		}
-		return null;
-	}
-	
+	/**Used to get the info of a player from the kag2d api by username
+	 * @param username The username of the player to look up
+	 * @return PlayerInfoObject An object containing the player info
+	 * @see https://developers.thd.vg/api/players.html
+	 * @see https://api.kag2d.com/v1/player/username/info
+	 */
 	public static PlayerInfoObject getPlayerInfo(String username)
 	{
 		String caseCheckURL = "https://api.kag2d.com/v1/player/"+username+"/info";
@@ -285,6 +399,12 @@ public class DiscordBot {
 		return null;
 	}
 	
+	/**Used to get the correct case of a players username given a string of their username with potentially erroneous upper/lower case. Returns an empty string if player not found, otherwise the correct string
+	 * @param username the username provided by someone that might have wrong case
+	 * @return An empty string if the player could not be found, otherwise the correct player string
+	 * @see https://developers.thd.vg/api/players.html
+	 * @see https://api.kag2d.com/v1/player/username/info
+	 */
 	public static String getCorrectCase(String username)
 	{
 		PlayerInfoObject info = getPlayerInfo(username);
@@ -292,6 +412,33 @@ public class DiscordBot {
 		else return info.username;
 	}
 	
+	public static List<PlayerObject> linkRequests;
+	
+	/**Helper function for checking for existing link requests
+	 * @param user the user to check for
+	 * @return the PlayerObject of the user if found, null if no player found
+	 * @see DiscordBot#addLinkRequest(IUser, String)
+	 * @see DiscordBot#doLinkRequest(String, IUser)
+	 * @see DiscordBot#doLinkRequest(String, long)
+	 */
+	public static PlayerObject getDiscordLinkRequest(IUser user)
+	{
+		for(PlayerObject p : linkRequests)
+		{
+			if(p.getDiscordUserInfo().equals(user))
+			{
+				return p;
+			}
+		}
+		return null;
+	}
+	
+	/**Adds the first half of a link request to link the discord user and KAG user, this is called by the !linkserver command when a player wants to start the server linking process.
+	 * @param user to be linked
+	 * @param kagname to be linked
+	 * @return 0 if updated an existing link request, 1 if a new link request was added
+	 * @see CommandLinkServer
+	 */
 	public static int addLinkRequest(IUser user, String kagname)
 	{
 		//check for an existing request
@@ -307,6 +454,14 @@ public class DiscordBot {
 		return 1;
 	}
 
+	/** Complete a server link request checks if the first half of the request already exists and the details are correct, then links the accounts. 
+	 * @param kagname to be linked
+	 * @param user to be linked
+	 * @return Returns -1 if the first half of the link request doesnt already exist, -2 if the first half of the link request is for a different account, and 1 if the link was successful
+	 * @see GatherDB#linkAccounts(String, long)
+	 * @see DiscordBot#addLinkRequest(IUser, String)
+	 * @see DiscordBot#doLinkRequest(String, long)
+	 */
 	public static int doLinkRequest(String kagname, IUser user)
 	{
 		//check if the other half of this request exists
@@ -329,6 +484,12 @@ public class DiscordBot {
 		
 	}
 
+	/**Wrapper function for {@link #doLinkRequest(String, IUser)}. Converts the id to an IUser object. Returns -3 if the user wasn't found. 
+	 * @param kagname to be linked
+	 * @param id to be linked
+	 * @return Returns -1 if the first half of the link request doesnt already exist, -2 if the first half of the link request is for a different account, 1 if the link was successful, and -3 if user could not be found
+	 * @see DiscordBot#doLinkRequest(String, IUser)
+	 */
 	public static int doLinkRequest(String kagname, long id)
 	{
 		IUser user = client.getUserByID(id);
