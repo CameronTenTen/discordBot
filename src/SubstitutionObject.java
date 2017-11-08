2d09bd7bd956d80b712c4923797c96ce25c21338
cameron <cameronh1010@gmail.com>
2017-11-08 19:17:47 +1030

    Added Javadoc for SubstitutionObject

--- src/SubstitutionObject.java
+++ src/SubstitutionObject.java
@@ -1,4 +1,8 @@
 
+/**Object representing a substitution that was made. Contains a PlayerObject for the player replaced and the player subbing in, as well as the GatherGame object. 
+ * @author cameron
+ * @see CommandSub
+ */
 public class SubstitutionObject {
 
 	SubstitutionObject(PlayerObject beingReplaced, PlayerObject player, GatherGame game)
