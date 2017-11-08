ee2f796aaa465ddb145c5f456bbe852653951b5a
cameron <cameronh1010@gmail.com>
2017-11-08 18:39:49 +1030

    Added Javadoc for CommandClearQueue

--- src/CommandClearQueue.java
+++ src/CommandClearQueue.java
@@ -2,8 +2,19 @@ import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import sx.blah.discord.handle.obj.IMessage;
 
+/**
+ * Admin only command for clearing the current gather queue. Must be used in command channel.
+ * Calls GatherObject.clearQueue()
+ * @author cameron
+ * @see GatherObject#clearQueue()
+ */
 public class CommandClearQueue implements CommandExecutor
 {
+	/**The function that is called when the command is used
+	 * @param message
+	 * @see https://github.com/BtoBastian/sdcf4j
+	 * @see #CommandClearQueue
+	 */
 	@Command(aliases = {"!clearqueue"}, description = "Admin only - clear the queue")
 	public void onCommand(IMessage message)
 	{
