8775538b07b7a2d03d6887d0fe43b4a071ae809c
cameron <cameronh1010@gmail.com>
2017-11-08 18:40:24 +1030

    Added Javadoc for CommandEnd

--- src/CommandEnd.java
+++ src/CommandEnd.java
@@ -2,8 +2,19 @@ import de.btobastian.sdcf4j.Command;
 import de.btobastian.sdcf4j.CommandExecutor;
 import sx.blah.discord.handle.obj.IMessage;
 
+/**
+ * Admin only command for moving all players out of the red and blue voice channels into the general channel. Must be used in command channel. 
+ * Calls GatherObject.movePlayersOutOfTeamRooms()
+ * @author cameron
+ * @see GatherObject#movePlayersOutOfTeamRooms()
+ */
 public class CommandEnd implements CommandExecutor
 {
+	/**The function that is called when the command is used
+	 * @param message
+	 * @see https://github.com/BtoBastian/sdcf4j
+	 * @see #CommandEnd
+	 */
 	@Command(aliases = {"!end"}, description = "Admin only - move users from team chat to general chat")
 	public void onCommand(IMessage message)
 	{
